65757cee13a2ca0668e176a2a1404dab0afa6dce
Jackie Wen <jzw22@cornell.edu>
2018-09-04 13:14:35 -0400

    Set args to variables, testing push

--- a1/src/meshgen/MeshGen.java
+++ a1/src/meshgen/MeshGen.java
@@ -1,43 +1,55 @@
-class MeshGen
+package meshgen;
+
+public class MeshGen
 {
     /**
      * Main method. Parses the command.
      * @param args
      */
+    
     public static void main(String[] args) {
-
+        
         if (args[0].equals("-g")) {
             // generate mesh if the first option is "-g"
             OBJMesh mesh = new OBJMesh();
+            String shape;
+            int divisionsU, divisionsV;
 
             for (int i=0; i < args.length; i += 2)
             {
                 if (args[i].equals("-g"))
                 {
                     // parse whether it is sphere or cylinder from args[i+1]
+                    shape = args[i+1];
                 }
                 else if (args[i].equals("-n"))
                 {
                     // parse latitude divisions from args[i+1]
                     // use Integer.parseInt() for parsing integer parameter
+                    divisionsU = Integer.parseInt(args[i+1]);
                 } else if (args[i].equals("-m"))
                 {
                     // parse v divisions
+                    divisionsV = Integer.parseInt(args[i+1]);
                 } else if (args[i].equals("-o"))
                 {
                     // output file
                 }
             }
+            
+            //Call function to create meshes based on shape?
+            
+            
         } else {
             // User provides an input OBJ mesh file, which the program reads in.
             // The mesh is assumed to have no normals (if normals are included in the input file, they are ignored).
             // The program then generates approximate normals at each vertex as described below.
             // Writes the resulting mesh to the user-provided output file.
-            OBJMesh mesh = new OBJMesh(args[1]);
+            //OBJMesh mesh = new OBJMesh(args[1]);
 
             // Calculate normals
 
-            mesh.writeOBJ(args[3]);
+            //mesh.writeOBJ(args[3]);
         }
     }
 }
