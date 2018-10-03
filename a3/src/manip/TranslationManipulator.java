41d95a425f1ab1d5a0d0b2b5b5936a435eb591d3
Michelle Y Zhou <myz6@cornell.edu>
2018-10-02 21:56:18 -0400

    basic translation

--- a3/src/manip/TranslationManipulator.java
+++ a3/src/manip/TranslationManipulator.java
@@ -30,7 +30,19 @@ public class TranslationManipulator extends Manipulator {
 		// Note that the mouse positions are given in coordinates that are normalized to the range [-1, 1]
 		//   for both X and Y. That is, the origin is the center of the screen, (-1,-1) is the bottom left
 		//   corner of the screen, and (1, 1) is the top right corner of the screen.
-
+		Vector2 dir = new Vector2(curMousePos);
+		dir.sub(lastMousePos);
+		switch (this.axis) {
+			case X:
+				this.reference.translation.set(0, 3, this.reference.translation.get(0, 3) + dir.x);
+				break;
+			case Y:
+				this.reference.translation.set(1, 3, this.reference.translation.get(1, 3) + dir.y);
+				break;
+			case Z:
+				this.reference.translation.set(2, 3, this.reference.translation.get(2, 3) + dir.);
+				break;
+		}
 	}
 
 	@Override
